66bea6880c2f0994a5c97826faeb14e476deb638
Xavier Meyer <xmeyer@kleis.ch>
2021-04-22 14:39:48 +0200

    Updated configuration script.

--- modules/webapp/scripts/user-data.sh
+++ modules/webapp/scripts/user-data.sh
@@ -6,12 +6,14 @@ cd /tmp/web
 
 sudo apt-get update
 sudo apt-get install -y python3-pip
-
+sudo pip3 install petname
 
 cat > index.html <<EOF
 <h1>Hello, World</h1>
 <h2>My name is ${server_name}!</h2>
+<h2>My local name is $(petname -w 4)!</h2>
 EOF
+
 
 nohup python3 -m http.server ${server_port} --directory /tmp/web
 
