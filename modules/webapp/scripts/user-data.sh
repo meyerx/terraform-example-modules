04e429af638435c4058ee92fcad733ef332d2d85
Xavier Meyer <xmeyer@kleis.ch>
2021-04-22 16:13:18 +0200

    Fixed webserver configuration for v0.1.0.

--- modules/webapp/scripts/user-data.sh
+++ modules/webapp/scripts/user-data.sh
@@ -4,10 +4,6 @@ set -e
 mkdir -p /tmp/web
 cd /tmp/web
 
-sudo apt-get update
-sudo apt-get install -y python3-pip
-
-
 cat > index.html <<EOF
 <h1>Hello, World</h1>
 <h2>My name is ${server_name}!</h2>
